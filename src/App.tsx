ec212523c40093fd6327143691655f5aa8cf252a
Sarah Smandack <sarah.smandack@gmail.com>
2024-10-29 12:09:12 +0100

    push solution

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+import "./App.css";
+
+const sampleEmployee = {
+  name: {
+    first: "Charlie",
+    last: "Thompson",
+  },
+  email: "charlie.thompson@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+};
+
+function App() {
+  const [employee, setEmployee] = useState(sampleEmployee);
+
+  function getEmployee(){
+    fetch("https://randomuser.me/api?nat=en")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setEmployee(data.results[0]);
+    });
+  }
+
+  return (
+    <div className='App'>
+      <EmployeeCard employee={employee} />
+      <button type="button" onClick={getEmployee}>Get Employee</button>
+    </div>
+  );
+}
+
+export default App;
